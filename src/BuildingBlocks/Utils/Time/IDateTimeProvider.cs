6258546ca62bdd31ef8112788de26b11ab349265
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-02-14 16:05:11 -0600

    Add DateTime Utils in BuildingBlocks/Utils/Time

---
+++ src/BuildingBlocks/Utils/Time/IDateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils.Time
+{
+    public interface IDateTimeProvider
+    {
+        public DateTime Now();
+        public DateTime NowUtc();
+        public DateTimeOffset NowOffset();
+        public DateTimeOffset NowOffsetUtc();
+    }
+}
